1110535f345af9fc785749838342f2c57c0f6b9c
Jonatan B. Jåsund <jonatan.jasund@pm.me>
2025-01-16 13:18:10 +0100

    edit socials

--- src/consts.ts
+++ src/consts.ts
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "jonatan.jasund@gmail.com",
+    HREF: "mailto:jonatan.jasund@gmail.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "johnnybangbang",
+    HREF: "https://github.com/johnnybangbang/astro-sphere"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "jonatanjasund",
+    HREF: "https://www.linkedin.com/in/jonatanjasund/",
   },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
+  // { 
+  //   NAME: "Twitter",
+  //   ICON: "twitter-x",
+  //   TEXT: "",
+  //   HREF: "https://twitter.com/",
+  // },
 ]
 
