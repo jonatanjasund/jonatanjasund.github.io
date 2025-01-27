723b74727d02816f616feb7fdeb06e7ccc212bcf
Jonatan B. Jåsund <jonatan.jasund@pm.me>
2025-01-27 11:46:31 +0100

    configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jonatanjasund.github.io",
+  site: "https://jonatanjasund.no",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
